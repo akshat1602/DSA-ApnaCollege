063488becf6ea106c4925bf70c3897cb1c5e8129
Akshat Trivedi <131957541+akshat1602@users.noreply.github.com>
2026-03-05 16:08:38 +0530

    Add implementations for sliding window maximum, weakest soldier, and connect N ropes algorithms

---
+++ Heaps - Priority Queue/Sliding-window-max.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void slidingWindowMax(vector <int> arr, int K ){
+
+    priority_queue<pair<int, int>> pq; //sliding window : (arr[i], i);
+    
+    //1st window
+    for (int i = 0; i < K; i++)
+    {
+        pq.push(make_pair(arr[i], i)); //pushing in PQ
+    }
+    cout<< "Output : "<<pq.top().first<<" ";
+
+    for (int i = K; i < arr.size(); i++)
+    {
+        while(!pq.empty() && pq.top().second <= (i-K)){ //outside window condition
+            pq.pop();
+        }
+
+        pq.push(make_pair(arr[i], i));
+        cout<<pq.top().first<<" ";
+    }
+    cout<<endl;
+}
+
+
+int main() {
+    vector <int> arr = {1, 3, -1, -3, 5, 3, 6, 7};
+    int K = 3;
+
+    slidingWindowMax(arr, K);
+    
+    return 0;
+}
